1ad2c0471428eb162c24e4e4115241e0ad907816
aha-oretama <sekine_y_529@msn.com>
2021-02-19 19:58:34 +0900

    sort failureTests

--- src/components/failureTestList.tsx
+++ src/components/failureTestList.tsx
@@ -15,10 +15,12 @@ export const FailureTestList: React.FunctionComponent = () => {
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
 
+  const sortedData = data.sort((a, b) => b.total_failure_count / b.total_test_count - a.total_failure_count / a.total_test_count)
+
   return (
     <>
       <h1>Top 10 frequently failed tests in the last 30 days</h1>
-      {data.length === 0 ?
+      {sortedData.length === 0 ?
         <p>There are no failed test in the last 30 days</p>
       : <table>
           <tr>
@@ -27,11 +29,10 @@ export const FailureTestList: React.FunctionComponent = () => {
             <th>Failure count</th>
             <th>Execution count</th>
           </tr>
-          {data.map(d => (
+          {sortedData.map(d => (
             <tr>
               <td>{d.name || d.classname}</td>
               {/* Round off the number to 2 decimal places */}
-              {/*<td>{Math.round(d.total_failure_count / d.total_test_count * 100 / 100) * 100}%</td>*/}
               <td>{Math.round(d.total_failure_count / d.total_test_count * 100 * 100) / 100}%</td>
               <td>{d.total_failure_count}</td>
               <td>{d.total_test_count}</td>
