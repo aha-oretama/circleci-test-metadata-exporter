2cd0fc500621a3eb1877d42ee7d27452675eaa54
aha-oretama <sekine_y_529@msn.com>
2021-02-19 17:32:47 +0900

    convert json data type

--- server/routes/timeline.js
+++ server/routes/timeline.js
@@ -11,14 +11,25 @@ const timeline = async (req, res) => {
       'start_time',
       'stop_time',
       [db.Sequelize.fn('COUNT', db.Sequelize.col('*')), 'total_test_count'],
-      [db.Sequelize.fn('SUM', db.Sequelize.col('`test_meta_data`.run_time')), 'total_tests_run_time']
+      [db.Sequelize.fn('SUM', db.Sequelize.col('test_meta_data.run_time')), 'total_tests_run_time'],
+      [db.Sequelize.fn('SUM', db.Sequelize.literal('CASE WHEN test_meta_data.result = \'success\' THEN 1 ELSE 0 END')), 'total_success_count'],
+      [db.Sequelize.fn('SUM', db.Sequelize.literal('CASE WHEN test_meta_data.result = \'skipped\' THEN 1 ELSE 0 END')), 'total_skipped_count'],
+      [db.Sequelize.fn('SUM', db.Sequelize.literal('CASE WHEN test_meta_data.result = \'failure\' THEN 1 ELSE 0 END')), 'total_failure_count'],
     ],
     include: {association: db.job.associations.test_meta_data, attributes: []},
     group: ['build_num']
   });
 
-  res.send(jobs);
+  // total_success_count etc. returns string type. So convert the type here
+  // Maybe related to https://github.com/sidorares/node-mysql2/issues/795
+  const formatJobs = jobs.map(j => ({
+    ...j.dataValues,
+    total_tests_run_time: parseFloat(j.dataValues.total_tests_run_time),
+    total_success_count: parseInt(j.dataValues.total_success_count),
+    total_skipped_count: parseInt(j.dataValues.total_skipped_count),
+    total_failure_count: parseInt(j.dataValues.total_failure_count),
+  }))
+  res.send(formatJobs);
 }
 
-exports.countTimeline = countTimeline;
 exports.timeline = timeline;
